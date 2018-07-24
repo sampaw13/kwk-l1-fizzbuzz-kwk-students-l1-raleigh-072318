c472b7ace0ab132274730b8d9f2b4cd680ec1245
sampaw13 <samarielpaw@gmail.com>
2018-07-24 18:38:35 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(number)
+  if number % 3 ==0  && if number % 5== 0 
+return "fizzbuzz"
+
+elsif number % 3 ==0 
+  return "fizz"
+
+elsif number % 5 == 0 
+  return "buzz"
+
+else return nil
+end
+  
+end
