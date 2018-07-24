62f6519827a027dbda34f41f1587156d9fe0afe1
sampaw13 <samarielpaw@gmail.com>
2018-07-24 18:46:34 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@ def fizzbuzz(number)
   if number % 3 == 0  && if number % 5== 0 
 return "fizzbuzz"
 
-elsif number % 3 ==0 
+elsif number % 3 == 0 
   return "fizz"
 
 elsif number % 5 == 0 
   return "buzz"
-# elsif number % 15 ==0 
+# elsif number % 15 == 0 
 #     return "fizzbuzz"
 
  end
